d36c9f5f50b0651a994c7c68551a435c80fef71c
jussupovv <gazizovmark79@gmail.com>
2024-12-07 19:15:57 +0300

    Байгазы: ДОБАВИЛ ФАЙЛ module_baigazy.py; ФАЙЛ main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from module_baigazy import STUDENTS_GRADES, STUDENTS_LIST
+
+def show_students(students):
+  for i in students:
+    print(i)
+
+show_students(STUDENTS_LIST)
---
+++ module_baigazy.py
@@ -0,0 +1,17 @@
+STUDENT_LIST = [
+    "esen m.",
+    "altynai a.",
+    "azamat t.",
+    "kylych s.",
+    "arsen k.",
+]
+
+MAX_SCORE = 30
+
+STUDENT_GRADES = (
+    (15, MAX_SCORE),
+    (12, MAX_SCORE),
+    (30, MAX_SCORE),
+    (1, MAX_SCORE),
+    (99, MAX_SCORE),
+)
